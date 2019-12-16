e4cf382e0a8d42ee4587d71b510311a590f3ba92
DaniilGirskiy <https://github.com/DaniilGirskiy>
2019-12-16 11:53:07 +0300

    Initial Commit

---
+++ DrinkUpSeguesWithCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrinkUpSeguesWithCode
+//
+//  Created by Danya on 16/12/2019.
+//  Copyright © 2019 Daniil Girskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
